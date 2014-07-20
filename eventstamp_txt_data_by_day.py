a79e614c866d9511d419af8865ffcac5e1431e5e
Sage Berg <sberg@uoregon.edu>
2014-07-20 09:04:49 -0700

    turned into a class, fixed some output formatting, still need to fix wrong duration on first stamp error

--- eventstamp_txt_data_by_day.py
+++ eventstamp_txt_data_by_day.py
@@ -15,116 +15,128 @@ try:
 except:
     print('failed to import personal_people_list.py')
 
-eventstamp_list = make_eventstamp_list()
-date_dict       = make_date_dict()
+class Data_By_Day(object):
+
+    def __init__(self):
+        self.eventstamp_list = make_eventstamp_list()
+        self.date_dict       = make_date_dict()
+
+    def make_date_string(self, index):
+        date_string = str(self.date_dict[index][1]) + '/'   + \
+                      str(self.date_dict[index][2]) + '/20' + \
+                      str(self.date_dict[index][0])
+        return date_string
     
-def make_time_use_by_day_file(): #not giving right time for first stamp
-    time_use_by_day = open('data/time_use_by_day.txt', 'w') 
-    for activity in event_list:
-        time_use_by_day.write(activity[0] + ',') 
-        #write column names to text file
-    time_use_by_day.write('\n')
-    for i in range(len(date_dict)): 
-        daily_time_use_dict = {activity[0].title():0 \
-                               for activity in event_list}
-        for j in range(len(eventstamp_list)): #stupid loop, fix
-            if date_dict[i] == eventstamp_list[j].date:
-                duration = get_eventstamp_duration(j, eventstamp_list)
-                daily_time_use_dict[eventstamp_list[j].what.strip()] \
-                += duration
-        daily_activity_list = [(activity[0].title(), \
-        daily_time_use_dict[activity[0].title()]) \
-                            for activity in event_list]
-        for tup in daily_activity_list: 
-            time_use_by_day.write(str(tup[1]) + ',')
+    def make_time_use_by_day_file(self): 
+        time_use_by_day = open('data/time_use_by_day.txt', 'w') 
+        time_use_by_day.write('DATE, ') 
+        for activity in event_list:
+            time_use_by_day.write(activity[0] + ',') 
+            #write column names to text file
         time_use_by_day.write('\n')
-    time_use_by_day.close()
 
-def make_happiness_by_day_file():
-    average_happiness_by_day = open('data/happiness_by_day.txt', 'w')
-    for i in range(len(date_dict) -1): 
-        #to do: special case: calculate average for current day 
-        date_string = str(date_dict[i][1]) + '/'   + \
-                      str(date_dict[i][2]) + '/20' + \
-                      str(date_dict[i][0])
-        average_happiness_by_day.write(date_string + ',')
-        day_happiness_sum = 0
-        divisor = 0
-        for j in range(1, len(eventstamp_list)):
-            if date_dict[i] == eventstamp_list[j].date and \
-               eventstamp_list[j].what != 'Sleep':
-                duration = get_eventstamp_duration(j, eventstamp_list)
-                day_happiness_sum += \
-                int(eventstamp_list[j].happiness.strip())*duration
-                divisor += duration
-        average = day_happiness_sum/divisor
-        average = round(average,3)
-        average_happiness_by_day.write(str(average) + '\n')
-    average_happiness_by_day.close()
+        for i in range(len(self.date_dict)): 
+            date_string = self.make_date_string(i)
+            time_use_by_day.write(date_string + ',')
+            daily_time_use_dict = {activity[0].title():0 \
+                                   for activity in event_list}
+            for j in range(len(self.eventstamp_list)): #stupid loop, fix
+                if self.date_dict[i] == self.eventstamp_list[j].date:
+                    duration = get_eventstamp_duration(j, self.eventstamp_list)
+                    daily_time_use_dict[self.eventstamp_list[j].what.strip()] \
+                    += duration
+            daily_activity_list = [(activity[0].title(), \
+            daily_time_use_dict[activity[0].title()]) \
+                                for activity in event_list]
+            for tup in daily_activity_list: 
+                time_use_by_day.write(str(tup[1]) + ',')
+            time_use_by_day.write('\n')
+        time_use_by_day.close()
 
-def make_people_time_dict(people_list): 
-    '''called by make_people_by_day_file'''
+    def make_happiness_by_day_file(self):
+        average_happiness_by_day = open('data/happiness_by_day.txt', 'w')
+        for i in range(len(self.date_dict)): 
+            date_string = self.make_date_string(i)
+            average_happiness_by_day.write(date_string + ',')
+            day_happiness_sum = 0
+            divisor = 0
+            for j in range(1, len(self.eventstamp_list)):
+                if self.date_dict[i] == self.eventstamp_list[j].date and \
+                   self.eventstamp_list[j].what != 'Sleep':
+                    duration = get_eventstamp_duration(j, self.eventstamp_list)
+                    day_happiness_sum += \
+                    int(self.eventstamp_list[j].happiness.strip())*duration
+                    divisor += duration
+            try:
+                average = day_happiness_sum/divisor
+            except:
+                print('average happiness = day_hapiness_sum/zero')
+            average = round(average,3)
+            average_happiness_by_day.write(str(average) + '\n')
+        average_happiness_by_day.close()
 
-    return {person : 0 for person in people_list}
+    def make_people_time_dict(self, people_list): 
+        '''called by make_people_by_day_file'''
 
-def make_people_by_day_file(): 
-    people_by_day = open('data/people_by_day.txt', 'w')
-    days_dict = dict()
-    for i in range(len(eventstamp_list)):
-        if eventstamp_list[i].date not in days_dict:
-            days_dict[eventstamp_list[i].date] = \
-            make_people_time_dict(people)
-        for person in eventstamp_list[i].who.split():
-            if person in make_people_time_dict(people):
-                duration = get_eventstamp_duration(i, eventstamp_list)
-                days_dict[eventstamp_list[i].date][person] += duration 
-    people_columns_string = "DATE, "
-    for person in people:
-        people_columns_string += person + ', '
-    people_by_day.write(people_columns_string[:-2] + '\n')
-    for i in range(len(date_dict)):
-        date_string = str(date_dict[i]) + ', '
+        return {person : 0 for person in people_list}
+
+    def make_people_by_day_file(self): 
+        people_by_day = open('data/people_by_day.txt', 'w')
+        days_dict = dict()
+        for i in range(len(self.eventstamp_list)):
+            if self.eventstamp_list[i].date not in days_dict:
+                days_dict[self.eventstamp_list[i].date] = \
+                self.make_people_time_dict(people)
+            for person in self.eventstamp_list[i].who.split():
+                if person in self.make_people_time_dict(people):
+                    duration = get_eventstamp_duration(i, self.eventstamp_list)
+                    days_dict[self.eventstamp_list[i].date][person] += duration 
+        people_columns_string = 'DATE, ' 
         for person in people:
-            date_string += str(days_dict[date_dict[i]][person]) + ', '
-        people_by_day.write(date_string[:-2] + '\n')
-    people_by_day.close()
+            people_columns_string += person + ', '
+        people_by_day.write(people_columns_string[:-2] + '\n')
+        for i in range(len(self.date_dict)):
+            date_string = str(self.date_dict[i]) + ', '
+            for person in people:
+                date_string += str(days_dict[self.date_dict[i]][person]) + ', '
+            people_by_day.write(date_string[:-2] + '\n')
+        people_by_day.close()
 
-def make_stress_by_day_file(): 
-    stress_by_day = open('data/stress_by_day.txt', 'w') 
-    for i in range(len(date_dict)): 
-        date_string = str(date_dict[i][1]) + '/'   + \
-                      str(date_dict[i][2]) + '/20' + \
-                      str(date_dict[i][0])
-        stress_by_day.write(date_string + ', ')
-        stress_minute_sum = 0
-        for j in range(1, len(eventstamp_list)): 
-            if date_dict[i] == eventstamp_list[j].date and \
-               eventstamp_list[j].stress == 1:
-                duration = get_eventstamp_duration(i, eventstamp_list)
-                stress_minute_sum += duration
-        stress_by_day.write(str(stress_minute_sum) + '\n')
-    stress_by_day.close()
+    def make_stress_by_day_file(self): 
+        stress_by_day = open('data/stress_by_day.txt', 'w') 
+        for i in range(len(self.date_dict)): 
+            date_string = self.make_date_string(i)
+            stress_by_day.write(date_string + ', ')
+            stress_minute_sum = 0
+            for j in range(1, len(self.eventstamp_list)): 
+                if self.date_dict[i] == self.eventstamp_list[j].date and \
+                   self.eventstamp_list[j].stress == 1:
+                    duration = get_eventstamp_duration(i, self.eventstamp_list)
+                    stress_minute_sum += duration
+            stress_by_day.write(str(stress_minute_sum) + '\n')
+        stress_by_day.close()
 
-def make_stamps_by_day_file():
-    f = open('data/stamps_by_day.txt', 'w')
-    fragment_dict = dict()
-    for eventstamp in eventstamp_list:
-        if eventstamp.date not in fragment_dict:
-            fragment_dict[eventstamp.date] = 1
-        else:
-            fragment_dict[eventstamp.date] += 1
-    date_list = list()
-    for key in fragment_dict:
-        date_list.append([key, fragment_dict[key]])
-    date_list.sort()
-    for date in date_list:
-        f.write(str(date[0][2]) + "-" + str(month_dict[date[0][1]]) + \
-        "-20" + str(date[0][0]) + ", " + str(date[1]) + "\n") 
-    f.close()
+    def make_stamps_by_day_file(self):
+        f = open('data/stamps_by_day.txt', 'w')
+        fragment_dict = dict()
+        for eventstamp in self.eventstamp_list:
+            if eventstamp.date not in fragment_dict:
+                fragment_dict[eventstamp.date] = 1
+            else:
+                fragment_dict[eventstamp.date] += 1
+        date_list = list()
+        for key in fragment_dict:
+            date_list.append([key, fragment_dict[key]])
+        date_list.sort()
+        for date in date_list:
+            f.write(str(date[0][2]) + "-" + str(month_dict[date[0][1]]) + \
+            "-20" + str(date[0][0]) + ", " + str(date[1]) + "\n") 
+        f.close()
 
 def main():
-    make_happiness_by_day_file()
-    make_stress_by_day_file()
-    make_time_use_by_day_file()
-    make_stamps_by_day_file()
-    make_people_by_day_file()
+    x = Data_By_Day()
+    x.make_happiness_by_day_file()
+    x.make_stress_by_day_file()
+    x.make_time_use_by_day_file()
+    x.make_stamps_by_day_file()
+    x.make_people_by_day_file()
